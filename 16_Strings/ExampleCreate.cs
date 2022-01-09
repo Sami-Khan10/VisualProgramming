6634c601b37ffbae9a05ea1d0f6595c9b23418f7
Muhammad Sami <77782070+Sami-Khan10@users.noreply.github.com>
2022-01-09 20:45:13 +0000

    Update ExampleCreate.cs
    On Line 33 It Should be chat

--- 16_Strings/ExampleCreate.cs
+++ 16_Strings/ExampleCreate.cs
@@ -30,7 +30,7 @@ namespace _16_Strings
             //formatting method to convert a value
             DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
             string chat = String.Format("Message sent at {0:t} on {0:D}",waiting);
-            Console.WriteLine("Message: {0}", chat);
+            Console.WriteLine("chat: {0}", chat);
 
             Console.ReadKey();
         }
